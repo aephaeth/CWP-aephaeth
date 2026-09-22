20e5aa60bfb9ebadacbed8bba702735d4c019fd9
Aekkachai Phaethong <aephaeth@z2t18c4.42bangkok.com>
2026-09-22 13:05:07 +0700

    Clean up duplicate ex00 and ex01 folders

--- ex00/name.py
+++
@@ -1,3 +0,0 @@
-first_name = "aekkachai"
-last_name = "Phaethong"
-print(first_name,last_name)
--- ex01/name.py
+++
@@ -1,4 +0,0 @@
-first_name = "aekkachai"
-last_name = "Phaethong"
-whole_name = last_name + first_name
-print(whole_name)
