bf544c21c11f563acae1752954b9b99e53beb38d
Aekkachai Phaethong <aephaeth@z2t18c4.42bangkok.com>
2026-09-23 09:16:37 +0700

    ex03

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -4,11 +4,13 @@ import sys
 if len(sys.argv) > 1:
     print("none")
     sys.exit()
-    
+
 i = 0
 while i <= 10:
     j = 0
+    line = f"Table de {i}:"
     while j <= 10:
-        print(f"{i} x {j} = {i * j}")
+        line += f" {i * j}"  
         j += 1
+    print(line)            
     i += 1
